e51c151f563890e33ac2e12515fbe6698419a93b
suzannes1434 <suzannestitt@gmail.com>
2024-08-14 15:08:57 -0700

    minor color changes

--- eden/lib/themes/dark_mode.dart
+++ eden/lib/themes/dark_mode.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
-    surface: Colors.grey.shade900,
+    surface: Colors.brown.shade800,
     primary: Colors.grey.shade600,
     secondary: Colors.grey.shade800,
     inversePrimary: Colors.grey.shade300
